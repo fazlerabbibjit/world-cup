d49c124b7263e3bb066c6e4e553adb72f6ee3e32
Fazle Rabbi Khan <fazle.rabbi@bjitacademy.com>
2022-12-06 14:31:00 +0600

    logo names updated

--- app/src/main/java/com/fazlerabbikhan/worldcup/datasorce/Datasource.kt
+++ app/src/main/java/com/fazlerabbikhan/worldcup/datasorce/Datasource.kt
@@ -8,12 +8,12 @@ class Datasource {
         return listOf<WorldCup>(
             WorldCup(R.drawable.netherlands_logo, R.string.team1, R.string.uefa),
             WorldCup(R.drawable.argentina_logo, R.string.team2, R.string.conmebol),
-            WorldCup(R.drawable.croatian_logo,R.string.team3, R.string.uefa),
-            WorldCup(R.drawable.brazilian_logo,R.string.team4, R.string.conmebol),
+            WorldCup(R.drawable.croatia_logo,R.string.team3, R.string.uefa),
+            WorldCup(R.drawable.brazil_logo,R.string.team4, R.string.conmebol),
             WorldCup(R.drawable.england_logo,R.string.team5, R.string.uefa),
             WorldCup(R.drawable.france_logo,R.string.team6, R.string.uefa),
             WorldCup(R.drawable.spain_logo,R.string.team7, R.string.uefa),
-            WorldCup(R.drawable.portuguese_logo,R.string.team8, R.string.uefa)
+            WorldCup(R.drawable.portugal_logo,R.string.team8, R.string.uefa)
         )
     }
 }
