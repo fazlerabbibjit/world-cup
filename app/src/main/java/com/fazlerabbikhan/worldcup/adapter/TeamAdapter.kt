29f7a008fd73d17cb69da1bf744cc60522be1069
Saiful Islam <saiful.islam@bjitacademy.com>
2022-12-06 15:04:31 +0600

    other activity add

--- app/src/main/java/com/fazlerabbikhan/worldcup/adapter/TeamAdapter.kt
+++ app/src/main/java/com/fazlerabbikhan/worldcup/adapter/TeamAdapter.kt
@@ -1,12 +1,14 @@
 package com.fazlerabbikhan.worldcup.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.fazlerabbikhan.worldcup.DetailsInfo
 import com.fazlerabbikhan.worldcup.R
 import com.fazlerabbikhan.worldcup.model.WorldCup
 
@@ -32,6 +34,14 @@ class TeamAdapter(
         holder.logo.setImageResource(item.logo)
         holder.name.text = context.resources.getString(item.name)
         holder.confederation.text = context.resources.getString(item.confederation)
+
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context, DetailsInfo::class.java)
+            intent.putExtra("teamLogo", item.logo.toString())
+            intent.putExtra("teamName", item.name.toString())
+            intent.putExtra("teamConfederation", item.confederation.toString())
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount() = dataset.size
